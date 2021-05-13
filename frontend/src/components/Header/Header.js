56d3c78396cdd4a014f83d3281748fb27bd40940
Đức Nguyễn <ntd275@gmail.com>
2021-05-13 22:22:09 +0700

    add header and footer

---
+++ frontend/src/components/Header/Header.js
@@ -0,0 +1,205 @@
+import React from "react";
+import { Navbar, Nav, NavDropdown } from "react-bootstrap"
+import { Link } from 'react-router-dom'
+import { FaHome, FaUserCircle } from 'react-icons/fa'
+import { BsGrid3X3Gap, BsFillLockFill, BsSearch } from 'react-icons/bs'
+import { FiMenu } from 'react-icons/fi'
+import { BiLineChart } from 'react-icons/bi'
+import '../../css/Header.css'
+
+class Header extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            iconSize: 20
+        }
+    }
+    render() {
+        return (
+            <header>
+                <Navbar bg="primary" expand="lg" variant="dark">
+                    <Link to="/" className="navbar-brand">
+                        <FaHome size={this.state.iconSize * 1.5} />
+                    </Link>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="mr-auto">
+                            <NavDropdown
+                                title={
+                                    <div className="d-inline-block">
+                                        <div className="position-relative">
+                                            <BsGrid3X3Gap size={this.state.iconSize} className="position-relative title-icon" />
+                                            <div className="title-text d-inline-block">
+                                                Danh mục
+                                            </div>
+                                        </div>
+                                    </div>
+                                }
+                                id="basic-nav-dropdown"
+                                menuAlign="right"
+                            >
+                                <Link to="/" className="dropdown-item">
+                                    Quản lý danh mục năm học
+                                </Link>
+                                <NavDropdown.Divider />
+                                <Link to="/" className="dropdown-item">
+                                    Quản lý danh mục lớp học
+                                </Link>
+                                <NavDropdown.Divider />
+                                <Link to="/" className="dropdown-item">
+                                    Quản lý danh mục môn học
+                                </Link>
+                                <NavDropdown.Divider />
+                                <Link to="/" className="dropdown-item">
+                                    Quản lý danh mục tổ chuyên môn
+                                </Link>
+                                <NavDropdown.Divider />
+                                <Link to="/" className="dropdown-item">
+                                    Quản lý danh mục học sinh
+                                </Link>
+                                <NavDropdown.Divider />
+                                <Link to="/" className="dropdown-item">
+                                    Quản lý danh mục giáo viên
+                                </Link>
+
+
+                            </NavDropdown>
+                            <NavDropdown
+                                title={
+                                    <div className="d-inline-block">
+                                        <div className="position-relative">
+                                            <FiMenu size={this.state.iconSize} className="position-relative title-icon" />
+                                            <div className="title-text d-inline-block">
+                                                Chức năng
+                                            </div>
+                                        </div>
+                                    </div>
+                                }
+                                id="basic-nav-dropdown"
+                            >
+                                <Link to="/" className="dropdown-item">
+                                    Phân công giảng dạy
+                                </Link>
+                                <NavDropdown.Divider />
+                                <Link to="/" className="dropdown-item">
+                                    Phân công GVCN
+                                </Link>
+                                <NavDropdown.Divider />
+                                <Link to="/" className="dropdown-item">
+                                    Phân công tổ chuyên môn
+                                </Link>
+                                <NavDropdown.Divider />
+                                <Link to="/" className="dropdown-item">
+                                    Quản lý danh sách học sinh trong lớp
+                                </Link>
+                                <NavDropdown.Divider />
+                                <Link to="/" className="dropdown-item">
+                                    Kết chuyển lớp
+                                </Link>
+                                <NavDropdown.Divider />
+                                <Link to="/" className="dropdown-item">
+                                    Quản lý tài khoản
+                                </Link>
+                            </NavDropdown>
+                            <NavDropdown
+                                title={
+                                    <div className="d-inline-block">
+                                        <div className="position-relative">
+                                            <BiLineChart size={this.state.iconSize} className="position-relative title-icon" />
+                                            <div className="title-text d-inline-block">
+                                                Thống kê
+                                            </div>
+                                        </div>
+                                    </div>
+                                }
+                                id="basic-nav-dropdown"
+                            >
+                                <Link to="/" className="dropdown-item">
+                                    Thông kê danh sách theo hạng
+                                </Link>
+                                <NavDropdown.Divider />
+                                <Link to="/" className="dropdown-item">
+                                    Thống kê kết quả theo lớp
+                                </Link>
+                                <NavDropdown.Divider />
+                                <Link to="/" className="dropdown-item">
+                                    Thống kê điểm bài thi theo cá nhân
+                                </Link>
+                            </NavDropdown>
+                            <Link to="/" className="nav-link">
+                                <div className="d-inline-block">
+                                    <div className="position-relative">
+                                        <BsFillLockFill size={this.state.iconSize} className="position-relative title-icon" />
+                                        <div className="title-text d-inline-block">
+                                            Khóa/Mở nhập điểm
+                                            </div>
+                                    </div>
+                                </div>
+                            </Link>
+                            <NavDropdown
+                                title={
+                                    <div className="d-inline-block">
+                                        <div className="position-relative">
+                                            <BsSearch size={this.state.iconSize} className="position-relative title-icon" />
+                                            <div className="title-text d-inline-block">
+                                                Tra cứu
+                                            </div>
+                                        </div>
+                                    </div>
+                                }
+                                id="basic-nav-dropdown"
+                            >
+                                <Link to="/" className="dropdown-item">
+                                    Tra cứu thông tin giáo viên
+                                </Link>
+                                <NavDropdown.Divider />
+                                <Link to="/" className="dropdown-item">
+                                    Tra cứu thông tin học sinh
+                                </Link>
+                                <NavDropdown.Divider />
+                                <Link to="/" className="dropdown-item">
+                                    Tra cứu phân công GVCN
+                                </Link>
+                                <NavDropdown.Divider />
+                                <Link to="/" className="dropdown-item">
+                                    Tra cứu phân công giảng dạy
+                                </Link>
+                            </NavDropdown>
+                        </Nav>
+                        <Nav>
+                            <NavDropdown
+                                alignRight
+                                title={
+                                    <div className="d-inline-block">
+                                        <div className="position-relative">
+                                            <FaUserCircle size={this.state.iconSize} className="position-relative title-icon" />
+                                            <div className="title-text d-inline-block">
+                                                Tài khoản
+                                            </div>
+                                        </div>
+                                    </div>
+                                }
+                                id="basic-nav-dropdown"
+                                className="dropdown-menu-right"
+                            >
+                                <Link to="/" className="dropdown-item">
+                                    Xem thông tin tài khoản
+                                </Link>
+                                <NavDropdown.Divider />
+                                <Link to="/" className="dropdown-item">
+                                    Đổi mật khẩu
+                                </Link>
+                                <NavDropdown.Divider />
+                                <Link to="/" className="dropdown-item">
+                                    Đăng xuất
+                                </Link>
+                            </NavDropdown>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+            </header>
+        )
+    }
+}
+
+export default Header
--- frontend/src/components/Login/login.js
+++ frontend/src/components/Login/login.js
@@ -2,10 +2,10 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Link } from "react-router-dom";
 
 class Login extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
-            formData : {},
+            formData: {},
         };
     }
     changeHandler = (e) => {
@@ -22,38 +22,36 @@ class Login extends Component {
     }
     render() {
         return (
-            <Router>
-                <div className="container">
-                    <div className="row" style={{ marginTop: "40px" }}>
-                        <div className="col-12 text-center">
-                            <h1>Hệ thống sổ liên lạc điện tử</h1>
-                            <h1>Trường Trung học cơ sở ABC</h1>
-                        </div>
-                    </div>
-                    <div className="row" style={{ marginTop: "40px" }} >
-                        <div className="col-6" style={{ margin: "auto" }}>
-                            <form action="/login" onSubmit={e => this.submitHandler(e)}>
-                                <div className="form-group">
-                                    <label >Tên tài khoản:</label>
-                                    <input type="text" className="form-control" id="username" 
-                                        name="username" 
-                                        onChange={e => this.changeHandler(e)}
-                                    />
-                                </div>
-                                <div className="form-group">
-                                    <label >Mật khẩu:</label>
-                                    <input type="password" className="form-control" id="pwd" 
-                                        name="password" 
-                                        onChange={e => this.changeHandler(e)}
-                                    />
-                                </div>
-                                <button type="submit" className="btn btn-primary" style={{ marginRight: "10px" }}>Đăng nhập</button>
-                                <Link to="/forgetpassword">Quên mật khẩu</Link>
-                            </form>
-                        </div>  
+            <div className="container">
+                <div className="row" style={{ marginTop: "40px" }}>
+                    <div className="col-12 text-center">
+                        <h1>Hệ thống sổ liên lạc điện tử</h1>
+                        <h1>Trường Trung học cơ sở ABC</h1>
                     </div>
                 </div>
-            </Router>
+                <div className="row" style={{ marginTop: "40px" }} >
+                    <div className="col-6" style={{ margin: "auto" }}>
+                        <form action="/login" onSubmit={e => this.submitHandler(e)}>
+                            <div className="form-group">
+                                <label >Tên tài khoản:</label>
+                                <input type="text" className="form-control" id="username"
+                                    name="username"
+                                    onChange={e => this.changeHandler(e)}
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label >Mật khẩu:</label>
+                                <input type="password" className="form-control" id="pwd"
+                                    name="password"
+                                    onChange={e => this.changeHandler(e)}
+                                />
+                            </div>
+                            <button type="submit" className="btn btn-primary" style={{ marginRight: "10px" }}>Đăng nhập</button>
+                            <Link to="/forgetpassword">Quên mật khẩu</Link>
+                        </form>
+                    </div>
+                </div>
+            </div>
         );
     }
 }
