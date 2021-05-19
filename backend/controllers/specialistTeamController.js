122deea75837c6d1e5d12bbd896ea23c21978728
tuanbmhust <tuanbm.hust@gmail.com>
2021-05-19 16:17:49 +0700

    update specialist team

--- backend/controllers/specialistTeamController.js
+++ backend/controllers/specialistTeamController.js
@@ -5,6 +5,12 @@ async function getSpecialistTeamList(req, res) {
     try {
         let page = req.query.page || config.pageItem
         let perpage = req.query.perpage || config.perPageItem
+        let sTeamList = await SpecialistTeam.getSpecialistTeamList(page, perpage)
+
+        return res.status(200).json({
+            success: true,
+            result: sTeamList
+        })
     } catch (error) {
         console.log(error)
         return res.status(500).json({
@@ -15,19 +21,79 @@ async function getSpecialistTeamList(req, res) {
 }
 
 async function getSpecialistTeam(req, res) {
-
+    try {
+        let sTeam = await SpecialistTeam.getSpecialistTeam(req.params.id)
+        return res.status(200).json({
+            success: true,
+            result: sTeam
+        })
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            success: false,
+            message: err
+        })
+    }
 }
 
 async function createSpecialistTeam(req, res) {
-    
+    try {
+        let sTeam = await SpecialistTeam.createSpecialistTeam(req.body)
+        return res.status(200).json({
+            success: true,
+            result: sTeam
+        })
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            success: false,
+            message: err
+        })
+    }
+
 }
 
 async function deleteSpecialistTeam(req, res) {
-    
+    try {
+        let count = await SpecialistTeam.deleteSpecialistTeam(req.params.id)
+        return res.status(200).json({
+            success: true,
+            result: count
+        })
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            success: false,
+            message: err
+        })
+    }
 }
 
 async function updateSpecialistTeam(req, res) {
-    
+    try {
+        let sTeam = await SpecialistTeam.getSpecialistTeam(req.params.id)
+        sTeam.specialistName = req.body.specialistName || sTeam.specialistName
+        sTeam.description = req.body.description || sTeam.description
+
+        let count = await Subject.updateSubject(subject)
+        if (count == 0) {
+            return res.status(404).json({
+                success: false,
+                message: "Specialist team not found"
+            })
+        }
+
+        return res.status(200).json({
+            success: true,
+            result: count
+        })
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            success: false,
+            message: err
+        })
+    }
 }
 
 module.exports = {
