63aa97a09b672e0735386573163aad8ed7dba03f
tuanbmhust <tuanbm.hust@gmail.com>
2021-05-22 15:46:07 +0700

    add class routes

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -3,11 +3,13 @@ const authRouter = require('./authRoutes')
 const accountRouter = require('./accountRoutes')
 const schoolYearRouter = require('./schoolYearRoutes')
 const specialistTeamRouter = require('./specialistTeamRoutes')
+const classRouter = require('./classRoutes')
 
 module.exports = function (app) {
   app.use('/auth', authRouter)
   app.use('/account', accountRouter)
   app.use('/school-year', schoolYearRouter)
   app.use('/specialist-team', specialistTeamRouter)
+  app.use('/class', classRouter)
   app.use(notFound)
 }
