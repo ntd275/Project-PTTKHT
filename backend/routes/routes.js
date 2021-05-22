2137a76933854336af7612cb2d6a62f58ca69f76
tuanbmhust <tuanbm.hust@gmail.com>
2021-05-22 14:58:14 +0700

    add sTeam route

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -2,10 +2,12 @@ const notFound = require('./404')
 const authRouter = require('./authRoutes')
 const accountRouter = require('./accountRoutes')
 const schoolYearRouter = require('./schoolYearRoutes')
+const specialistTeamRouter = require('./specialistTeamRoutes')
 
 module.exports = function (app) {
   app.use('/auth', authRouter)
   app.use('/account', accountRouter)
   app.use('/school-year', schoolYearRouter)
+  app.use('/specialist-team', specialistTeamRouter)
   app.use(notFound)
 }
---
+++ backend/routes/specialistTeamRoutes.js
@@ -0,0 +1,18 @@
+const sTeamRouter = require('express').Router()
+const sTeamController = require('../controllers/specialistTeamController')
+const authMiddleware = require('../middlewares/authentication')
+const adminMiddleware = require('../middlewares/administrator')
+const notFound = require('./404')
+
+sTeamRouter.use(authMiddleware.isAuth)
+
+sTeamRouter.get('/list', sTeamController.getSpecialistTeamList)
+sTeamRouter.get('/id/:id', sTeamController.getSpecialistTeam)
+
+// sTeamRouter.use(adminMiddleware)
+
+sTeamRouter.post('/', sTeamController.createSpecialistTeam)
+sTeamRouter.put('/id/:id', sTeamController.updateSpecialistTeam)
+sTeamRouter.delete('/id/:id', sTeamController.deleteSpecialistTeam)
+
+module.exports = sTeamRouter
