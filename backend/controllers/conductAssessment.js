878f6172246f29b94e9f8749e26c71e1e5ea18e0
tuanbmhust <tuanbm.hust@gmail.com>
2021-06-02 02:57:11 +0700

    update conduct

--- backend/controllers/conductAssessment.js
+++ backend/controllers/conductAssessment.js
@@ -1,7 +1,7 @@
 const Conduct = require('../models/Conduct')
 const config = require('../config/config')
 
-async function getConduct(req, res) {
+async function getStudentConduct(req, res) {
     try {
         let studentId = req.query.studentId
         let schoolYearId = req.query.schoolYearId
@@ -9,11 +9,8 @@ async function getConduct(req, res) {
 
         let conduct = await Conduct.getConduct(studentId, schoolYearId, term)
 
-        if (conduct == {} || conduct == null) {
-            return res.status(400).json({
-                success: false,
-                message: "Conduct not found"
-            })
+        if (conduct === undefined) {
+            conduct = {}
         }
 
         return res.status(200).json({
@@ -29,26 +26,26 @@ async function getConduct(req, res) {
         })
     }
 }
+async function getClassConduct(req, res) {
+    try {
+        let conducts = await Conduct.getClassConduct(req.query.classId, req.query.schoolYearId, req.query.term)
+
+        return res.status(200).json({
+            success: true,
+            result: conducts
+        })
+
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            success: false,
+            message: error
+        })
+    }
+}
 
 async function assessConduct(req, res) {
     try {
-        let conduct = await Conduct.getConduct(req.body.studentId, req.body.schoolYearId, req.body.term)
-
-        if (!conduct) {
-            let count = await Conduct.createConduct(req.body)
-
-            return res.status(200).json({
-                success: true,
-                result: count
-            })
-        }
-        // conduct.studentId = req.body.studentId || conduct.studentId
-        // conduct.classId = req.body.classId || conduct.classId
-        // conduct.teacherId = req.body.teacherId || conduct.teacherId,
-        // conduct.schoolYearId = req.body.schoolYearId || conduct.schoolYearId,
-        // conduct.conduct = req.body.conduct || conduct.conduct
-        // conduct.term = req.body.term || conduct.term
-
         //Check conduct validation
         if (req.body.conduct < 0 || req.body.conduct > 3) {
             return res.status(400).json({
@@ -57,7 +54,14 @@ async function assessConduct(req, res) {
             })
         }
 
-        let count = await Conduct.updateConduct(req.body)
+        let conduct = await Conduct.getConduct(req.body.studentId, req.body.schoolYearId, req.body.term)
+
+        let count = 0
+        if (conduct === undefined || !conduct) { //If not existed
+            count = await Conduct.createConduct(req.body)
+        } else { //If existed
+            count = await Conduct.updateConduct(req.body)
+        }
 
         return res.status(200).json({
             success: true,
@@ -74,6 +78,7 @@ async function assessConduct(req, res) {
 }
 
 module.exports = {
-    getConduct: getConduct,
+    getStudentConduct: getStudentConduct,
+    getClassConduct: getClassConduct,
     assessConduct: assessConduct
 }
